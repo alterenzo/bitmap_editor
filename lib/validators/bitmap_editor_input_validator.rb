6e8e9bafb2b54926eeaafd4000d994317986bfd4
Crescenzo D'Alterio <crescenzo.dalterio@gmail.com>
2018-02-13 00:01:25 +0000

    Refactors the BitmapEditorInputValidator

--- lib/validators/bitmap_editor_input_validator.rb
+++ lib/validators/bitmap_editor_input_validator.rb
@@ -2,27 +2,41 @@ require_relative '../ext/string.rb'
 require_relative '../errors/invalid_input_error.rb'
 
 module BitmapEditorInputValidator
-
   def validate_init_instruction(instr)
-    raise InvalidInputError.new('The init instruction must only contain 3 elements') if instr.split.length != 3
-    raise InvalidInputError.new('The dimensions must be integers') unless instr.split[1].is_i? && instr.split[2].is_i?
-    raise InvalidInputError.new('Dimensions must be > 1') unless instr.split[1].to_i > 0 && instr.split[2].to_i > 0
+    validate_number_of_elements(instr, 3)
+    validate_coordinates(instr.split[1,2])
   end
 
   def validate_color_pixel_instruction(instr)
-    raise InvalidInputError.new('The init instruction must only contain 4 elements') if instr.split.length != 4
-    raise InvalidInputError.new('The coordinates must be integers') unless instr.split[1].is_i? && instr.split[2].is_i?
-    raise InvalidInputError.new('Coordinates must be > 1') unless instr.split[1].to_i > 0 && instr.split[2].to_i > 0
+    validate_number_of_elements(instr, 4)
+    validate_coordinates(instr.split[1,2])
   end
 
   def validate_horizontal_line_instruction(instr)
-    raise InvalidInputError.new('The init instruction must only contain 5 elements') if instr.split.length != 5
-    raise InvalidInputError.new('The coordinates must be integers') unless instr.split[1].is_i? && instr.split[2].is_i? && instr.split[3].is_i?
-    raise InvalidInputError.new('Coordinates must be > 1') unless instr.split[1].to_i > 0 && instr.split[2].to_i > 0 && instr.split[3].to_i > 0
+    validate_number_of_elements(instr, 5)
+    validate_coordinates(instr.split[1,3])
   end
 
   def validate_vertical_line_instruction(instr)
     validate_horizontal_line_instruction(instr)
   end
 
+  private
+
+  def validate_number_of_elements(instr, length)
+    raise InvalidInputError.new("The instruction must only contain #{length} elements") if instr.split.length != length
+  end
+
+  def validate_coordinates(coordinates)
+    raise InvalidInputError.new('The coordinates must be integers') unless all_integers?(coordinates)
+    raise InvalidInputError.new('Coordinates must be > 1') unless all_bigger_than_one?(coordinates)
+  end
+
+  def all_integers?(strings)
+    strings.all? { |string| string.is_i? }
+  end
+
+  def all_bigger_than_one?(coordinates)
+    coordinates.all? { |coordinate| coordinate.to_i > 0 }
+  end
 end
